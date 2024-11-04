38963c89a438c45b13e3e2c0e24c8ca6ff55130b
Marco Miranda <marcomiranda566@gmail.com>
2024-11-04 02:29:26 -0600

    correcion de colores

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -9,7 +9,7 @@ const Layout = ({children}) => {
         <Navbar/>
         <div className='section1'></div>
         <div className="columns" style={{minHeight: "90vh", backgroundColor: '#ffffff'}} > 
-            <div className="column" style={{ backgroundColor: '#f0f0f0', opacity: 0.9 }}>
+            <div className="column" style={{ backgroundColor: '#0A0A0A', opacity: 0.9 }}>
                 <main>{children}</main>
             </div>               
         </div>
