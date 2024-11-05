f493288d91ecdb4a2166dfe0e81892790b10ff6e
Jose Angel Malo Martinez <malomtzjoseangel@gmail.com>
2024-11-05 12:47:26 -0600

    hola malo

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,87 +1,159 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { LoginUser, reset } from "../features/authSlice";
-import "../styles/Login.css"; 
-import { IoMail, IoLockClosed, IoLogIn } from "react-icons/io5"; 
+import eye from "../assets/eye.svg"; // importar imagenes
+import image1 from "../assets/img/img_1.jpeg";
+import image2 from "../assets/img/img_2.jpeg";
+import Slider from "react-slick"; // Importar Slider
+import "../styles/Login.css";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
+    
     const dispatch = useDispatch();
     const navigate = useNavigate();
-    const {user, isError, isSuccess, isLoading, message} = useSelector(
-      (state) => state.auth
+    const emailInputRef = useRef(null);
+    const { user, isError, isSuccess, isLoading, message } = useSelector(
+        (state) => state.auth
     );
 
-    useEffect(() =>{
-      if(user || isSuccess){
-        navigate("/dashboard");
-      }
-      dispatch(reset());
-    },[user, isSuccess, dispatch, navigate]);
+    useEffect(() => {
+        if (user || isSuccess) {
+            navigate("/dashboard");
+        }
+        dispatch(reset());
+    }, [user, isSuccess, dispatch, navigate]);
 
-    const Auth = (e) => {
-      e.preventDefault();
-      dispatch(LoginUser({email, password}));
+    useEffect(() => {
+        emailInputRef.current.focus();
+    }, []);
+
+    const handleAuth = (e) => {
+        e.preventDefault();
+        dispatch(LoginUser({ email, password }));
     };
 
-  return (
-    <section className="hero has-background-grey-light is-fullwidht" style={{ padding: '91px' }}>
-      <div className="hero-body">
-        <div className="container" style={{minHeight: "58vh"}}>
-          <div className="columns is-centered">
-            <div className="column is-4">             
-              <form 
-                onSubmit={Auth}
-                className='box has-background-light has-text-dark'
-              >              
-              <h1 className='has title has-text-dark'>Sign In</h1>
-                <div className="field">
-                  <label className='label has-text-dark'><IoMail /> Email:</label> 
-                  <div className="control">
-                    <input 
-                      type='text' 
-                      className="input2"
-                      required
-                      value={email}                  
-                      onChange={(e) => setEmail(e.target.value)}  
-                      placeholder='Email'             
-                    />
-                  </div>
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
+
+    const PrevButton = ({ onClick }) => (
+        <button className="prev-button" onClick={onClick}>
+            {/* Puedes añadir un icono o texto aquí */}
+            &#10094; {/* Este es un símbolo de flecha hacia la izquierda */}
+        </button>
+    );
+    
+    const NextButton = ({ onClick }) => (
+        <button className="next-button" onClick={onClick}>
+            {/* Puedes añadir un icono o texto aquí */}
+            &#10095; {/* Este es un símbolo de flecha hacia la derecha */}
+        </button>
+    );
+    
+
+    // Configuración del carrusel
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+    };
+
+    return (
+        <div className="main-container">
+        <div className="carrusel">
+            <Slider {...settings}
+            prevArrow={<PrevButton />}
+            nextArrow={<NextButton />}
+            style={{ height: '100%' }}> {/* Ajuste aquí */}
+                <div>
+                    <img src={image1} alt="Imagen 1" />
                 </div>
-                <div className="field">
-                  <label className='label has-text-dark'><IoLockClosed /> Password:</label> 
-                  <div className="control">
-                    <input 
-                      type='password' 
-                      className="input2" 
-                      required
-                      value={password}                  
-                      onChange={(e) => setPassword(e.target.value)}  
-                      placeholder='******'   
-                    />
-                  </div>
+                <div>
+                    <img src={image2} alt="Imagen 2" />
                 </div>
-                <div className="field mt-5">
-                  <button 
-                    type='submit' 
-                    className='button is-success is-fullwidth has-text-light'
-                    >
-                      {isLoading ? 'Succesfulled' : Login} Login <IoLogIn />
-                  </button>
-                  <div 
-                      className='loadingMsg'>
-                       { isError && <p className='has-text-centered'>{message}</p>}
-                  </div>
-                </div>
-              </form>
-            </div>
-          </div>
+            </Slider>
+            <p className="eslogan">¡Vive la emoción del Reto Puma! Únete a nosotros y demuestra tu pasión por el ciclismo.<br/>
+                    ¡Acepta el desafío y pedalea hacia la victoria!
+            </p>
         </div>
-      </div>
-    </section>
-  );
+            <div className="form">
+                <div className="title-section">
+                    <div className="text-wrapper">Bienvenido de vuelta!</div>
+                </div>
+                <form onSubmit={handleAuth}>
+                    <div className="inputs-section">
+                        {/* Campo de correo electrónico */}
+                        <div className="input2">
+                            <div className="textInputWrapper">
+                                <label htmlFor="email" className="email-label">Correo:</label>
+                                <input
+                                    ref={emailInputRef}
+                                    id="email"
+                                    type="email"
+                                    className="textInput"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    placeholder="Ingresa tu correo:"
+                                    required
+                                />
+                            </div>
+                        </div>
+
+                        {/* Campo de contraseña */}
+                        <div className="input-2">
+                            <div className="textInputWrapper">
+                                <label htmlFor="password">Ingresa tu contraseña:</label>
+                                <div className="input-container">
+                                    <input 
+                                        id="password"
+                                        type={showPassword ? "text" : "password"} 
+                                        className="textInput"
+                                        value={password}
+                                        onChange={(e) => setPassword(e.target.value)}
+                                        placeholder="Contraseña"
+                                        required
+                                    />
+                                    <img 
+                                        className="eye" 
+                                        alt="Eye" 
+                                        src={eye} 
+                                        onClick={togglePasswordVisibility}
+                                    />
+                                </div>
+                            </div>
+                            <div className="text-wrapper-3">¿Has olvidado tu contraseña?</div>
+                        </div>
+                    </div>
+                    {/* Mensaje de error */}
+                    {isError && <div className="error-message">{message}</div>}
+                    <div className="footer-section">
+                        {/* Botón de envío */}
+                        <button type="submit" className="buttom">
+                            <div className="text-wrapper-4">
+                                {isLoading ? 'Cargando...' : 'Iniciar sesión'}
+                            </div>
+                        </button>
+                        {/* Registro de cuenta */}
+                        <p className="a-n-no-tienes-cuenta">
+                            <span className="span">¿Aún no tienes cuenta? </span>
+                            {/* Para mandar al regidtro */}
+                            <span className="text-wrapper-5">Crea una ahora</span>
+                        </p>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
 };
 
 export default Login;
